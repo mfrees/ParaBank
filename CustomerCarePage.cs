4c5e214b7b562c6a4faed993d7e2de3bc8b65dd9
Michael Rees <38552546+mfrees@users.noreply.github.com>
2020-08-03 20:02:48 +0100

    Add files via upload

---
+++ CustomerCarePage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace ParaBank
+{
+    internal class CustomerCarePage : BaseApplicationPage
+    {
+        public CustomerCarePage(IWebDriver driver) :base(driver) { }
+        //below are the list of properties for the Customer Care Page
+        public IWebElement ContactButton => Driver.FindElement(By.LinkText("contact"));
+        public IWebElement NameField => Driver.FindElement(By.Id("name"));
+        public IWebElement EmailFiled => Driver.FindElement(By.Id("email"));
+        public IWebElement PhoneField => Driver.FindElement(By.Id("phone"));
+        public IWebElement MessageBox => Driver.FindElement(By.Id("message"));
+        public IWebElement SendToCutomerCareButton => Driver.FindElement(By.XPath("//*[@value='Send to Customer Care']"));
+
+
+        internal void FillOutFormAndSubmit(string nameField, string emailField, string phoneField, string messageField)
+        {
+            //Tidy this up next
+            ContactButton.Click(); //click the contact button in below welcome to ParaBank
+            NameField.SendKeys(nameField); //populates the name field
+            EmailFiled.SendKeys(emailField); //populates the email field
+            PhoneField.SendKeys(phoneField);//populates the phone field
+            MessageBox.SendKeys(messageField); //populates the message box
+            SendToCutomerCareButton.Click(); //clicks the send to customer care button
+        }
+    }
+}
