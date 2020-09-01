9b78b02597b48b47323d1efa77bfd606d459cc85
Michael Rees <michaelrees@outlook.com>
2020-09-01 21:42:19 +0100

    Added customer lookup test class and page objects class

---
+++ ParaBank/CustomerLookUpTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+
+namespace ParaBank
+{
+    [TestFixture]
+    public class CustomerLookUpTest : BaseTest1
+    {
+        [Description("Verifies the page sub heading.")]
+        [Author("Michael Rees")]
+        [Test]
+        public void VerifyPageText()
+        {
+            var customerlookuppage = new CustomerLookupPage(Driver);
+            customerlookuppage.VerifyPageText();
+
+            //Assert sub header
+            Assert.That("Customer Lookup", Is.EqualTo("Customer Lookup"));
+            //Assert sentence below sub header
+            Assert.That("Please fill out the following information in order to validate your account.", Is.EqualTo("Please fill out the following information in order to validate your account."));
+            //Assert field labels
+            Assert.That("First Name:", Is.EqualTo("First Name:"));
+            Assert.That("Last Name:", Is.EqualTo("Last Name:"));
+            Assert.That("Address:", Is.EqualTo("Address:"));
+            Assert.That("City:", Is.EqualTo("City:"));
+            Assert.That("State:", Is.EqualTo("State:"));
+            Assert.That("Zip Code:", Is.EqualTo("Zip Code:"));
+            Assert.That("SSN:", Is.EqualTo("SSN:"));
+            Assert.IsTrue(Driver.FindElement(By.XPath("//*[@value='Find My Login Info']")).Displayed);
+        }
+        [Description("Verify mandatory fields error's ")]
+        [Author("Michael Rees")]
+        [Test]
+        public void VerifyManadtoryFieldAlerts()
+        {
+            var customerlookuppage = new CustomerLookupPage(Driver);
+            customerlookuppage.VerifyMandatoryFieldWarnings();
+
+            Assert.That("First name is required.", Is.EqualTo("First name is required."));
+            Assert.That("Last name is required.", Is.EqualTo("Last name is required."));
+            Assert.That("Address is required.", Is.EqualTo("Address is required."));
+            Assert.That("City is required.", Is.EqualTo("City is required."));
+            Assert.That("State is required.", Is.EqualTo("State is required."));
+            Assert.That("Zip Code is required.", Is.EqualTo("Zip Code is required."));
+            Assert.That("Social Security Number is required.", Is.EqualTo("Social Security Number is required."));
+        }
+        
+    }
+}
---
+++ ParaBank/CustomerLookupPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+
+
+
+namespace ParaBank
+{
+    internal class CustomerLookupPage : BaseApplicationPage
+
+    {
+
+        public CustomerLookupPage(IWebDriver driver) : base(driver) { }
+
+        //Page objects below
+        public IWebElement ForgotLoginInfoHyperLink => Driver.FindElement(By.LinkText("Forgot login info?"));
+        public IWebElement FindMyLoginInfoButton => Driver.FindElement(By.XPath("//*[@value='Find My Login Info']"));
+
+
+        internal void VerifyPageText()
+        {
+            ForgotLoginInfoHyperLink.Click();
+        }
+
+        internal void VerifyMandatoryFieldWarnings()
+        {
+            ForgotLoginInfoHyperLink.Click();
+            FindMyLoginInfoButton.Click();
+        }
+    }
+}
