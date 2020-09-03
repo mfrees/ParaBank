e1daa394426ffd541763deaffd1b5ea57cea4aa6
Michael Rees <michaelrees@outlook.com>
2020-09-03 17:37:03 +0100

    Modified customerlookuptest

--- ParaBank/CustomerLookUpTest.cs
+++ ParaBank/CustomerLookUpTest.cs
@@ -18,19 +18,19 @@ namespace ParaBank
             var customerlookuppage = new CustomerLookupPage(Driver);
             customerlookuppage.VerifyPageText();
 
-            //Assert sub header
-            Assert.That("Customer Lookup", Is.EqualTo("Customer Lookup"));
-            //Assert sentence below sub header
-            Assert.That("Please fill out the following information in order to validate your account.", Is.EqualTo("Please fill out the following information in order to validate your account."));
-            //Assert field labels
-            Assert.That("First Name:", Is.EqualTo("First Name:"));
-            Assert.That("Last Name:", Is.EqualTo("Last Name:"));
-            Assert.That("Address:", Is.EqualTo("Address:"));
-            Assert.That("City:", Is.EqualTo("City:"));
-            Assert.That("State:", Is.EqualTo("State:"));
-            Assert.That("Zip Code:", Is.EqualTo("Zip Code:"));
-            Assert.That("SSN:", Is.EqualTo("SSN:"));
-            Assert.IsTrue(Driver.FindElement(By.XPath("//*[@value='Find My Login Info']")).Displayed);
+            Assert.Multiple(testDelegate: () =>
+            {
+                Assert.That("Customer Lookup", Is.EqualTo("Customer Lookup"));
+                Assert.That("Please fill out the following information in order to validate your account.", Is.EqualTo("Please fill out the following information in order to validate your account."));
+                Assert.That("First Name:", Is.EqualTo("First Name:"));
+                Assert.That("Last Name:", Is.EqualTo("Last Name:"));
+                Assert.That("Address:", Is.EqualTo("Address:"));
+                Assert.That("City:", Is.EqualTo("City:"));
+                Assert.That("State:", Is.EqualTo("State:"));
+                Assert.That("Zip Code:", Is.EqualTo("Zip Code:"));
+                Assert.That("SSN:", Is.EqualTo("SSN:"));
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@value='Find My Login Info']")).Displayed);
+            });
         }
         [Description("Verify mandatory fields error's ")]
         [Author("Michael Rees")]
@@ -41,6 +41,7 @@ namespace ParaBank
             customerlookuppage.VerifyMandatoryFieldWarnings();
 
             //One assertion per test rule is fine for unit tests, however as we progress up the testing stack there will be a need for multiple assertions.
+            //We use the Assert.Multiple and then pass a Lambda function which contains multiple assertion statements
             Assert.Multiple(testDelegate: () =>
              {
                  Assert.That("First name is required.", Is.EqualTo("First name is required."));
@@ -51,8 +52,6 @@ namespace ParaBank
                  Assert.That("Zip Code is required.", Is.EqualTo("Zip Code is required."));
                  Assert.That("Social Security Number is required.", Is.EqualTo("Social Security Number is required."));
              });
-
-            
         }
         
     }
