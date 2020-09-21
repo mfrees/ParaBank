7433d51acf03ac794ac2fe2a0f652af8022f8b79
Michael Rees <michaelrees@outlook.com>
2020-09-21 21:27:07 +0100

    Created FindTransactionsApplicationPage.CS and FindTransactionsTests.cs

---
+++ ParaBank/FindTransactionsApplicationPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace ParaBank
+{
+    internal class FindTransactionsApplicationPage :BaseApplicationPage
+    {
+        public FindTransactionsApplicationPage(IWebDriver driver): base(driver) { }
+
+        //Below are the list of properties 
+        public IWebElement FindTransactions => Driver.FindElement(By.LinkText("Find Transactions"));
+
+        //Below are the methods 
+        internal void VerifyPageText()
+        {
+            FindTransactions.Click();
+        }
+    }
+}
---
+++ ParaBank/FindTransactionsTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace ParaBank
+{
+    [TestFixture]
+    [Parallelizable]
+    [Category("Find Transactions"), Category("Regression Tests")]
+    public class FindTransactionsTest : BaseTest
+    {
+        internal FindTransactionsApplicationPage FindTransactionsApplicationPage { get; private set; } //This property is linked to the FindTransactionsApplicationPage in the Setup
+        [Test]
+        public void VerifyPageText()
+        {
+            FindTransactionsApplicationPage.VerifyPageText();
+
+            Assert.Multiple(testDelegate: () =>
+            {
+                //Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='title']")).Displayed); //Verifies that the page title is actual displayed
+                Assert.That("Find Transactions", Is.EqualTo("Find Transactions")); //Verifies the page title header text is displayed as expected
+            });
+        }
+        [SetUp]
+        public void RunsBeforeEachTest()
+        {
+            FindTransactionsApplicationPage = new FindTransactionsApplicationPage(Driver);
+        }
+    }
+}
