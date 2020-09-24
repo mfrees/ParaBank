5beb03299d12dd3ad814fb284da4697f217ed0fa
Michael Rees <michaelrees@outlook.com>
2020-09-24 22:16:10 +0100

    Added further assertions to FindTransactionsTest.CS

--- ParaBank/FindTransactionsTest.cs
+++ ParaBank/FindTransactionsTest.cs
@@ -16,8 +16,15 @@ namespace ParaBank
 
             Assert.Multiple(testDelegate: () =>
             {
-                //Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='title']")).Displayed); //Verifies that the page title is actual displayed
+                Assert.AreEqual("ParaBank | Find Transactions", Driver.Title);
                 Assert.That("Find Transactions", Is.EqualTo("Find Transactions")); //Verifies the page title header text is displayed as expected
+                Assert.That("Select an account", Is.EqualTo("Select an account"), "Verify field label called Select an account");
+                Assert.That("Find by Transaction ID", Is.EqualTo("Find by Transaction ID"), "Verify field label called Find by Transaction ID");
+                Assert.That("Find by Date", Is.EqualTo("Find by Date"), "Verify field label called Find by Date");
+                Assert.That("Find by Date Range", Is.EqualTo("Find by Date Range"), "Verify field label called Find by Date Range");
+                Assert.That("Find by Amount", Is.EqualTo("Find by Amount"), "Verify field label called Find by Amount");
+                //Below verifies the buttons 
+                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[3]/button")).Enabled);
             });
         }
         [SetUp]
