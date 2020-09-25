f9298df7d33e91621603c2bed50fb9bf29d0fc19
Michael Rees <michaelrees@outlook.com>
2020-09-25 15:38:55 +0100

    New test cases added to FindTransactionsTest.cs

--- ParaBank/FindTransactionsApplicationPage.cs
+++ ParaBank/FindTransactionsApplicationPage.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using System;
+using System.Threading;
 
 namespace ParaBank
 {
@@ -9,11 +10,77 @@ namespace ParaBank
 
         //Below are the list of properties 
         public IWebElement FindTransactions => Driver.FindElement(By.LinkText("Find Transactions"));
+        public IWebElement AccountList => Driver.FindElement(By.Id("accountId"));
+        public IWebElement FindByTransIDField => Driver.FindElement(By.Id("criteria.transactionId"));
+        public IWebElement FindTransactionsButtonID => Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[3]/button")); //Find Transactions button in the Find by Transaction ID section
+        public IWebElement FindByDateField => Driver.FindElement(By.Id("criteria.onDate"));
+        public IWebElement FindTransactionsButtonDate => Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[5]/button")); //Find Transactions button in the Find By Date section
+        public IWebElement DateRangeBetweenField => Driver.FindElement(By.Id("criteria.fromDate"));
+        public IWebElement DateRangeAndField => Driver.FindElement(By.Id("criteria.toDate"));
+
+        public IWebElement FindTransactionsButtonDateRange => Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[7]/button")); //Find Transactions button in the Find by Date Range section
+        public IWebElement FindByAmountField => Driver.FindElement(By.Id("criteria.amount"));
+        public IWebElement FindTransactionsButtonAmount => Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[9]/button")); //Find Transaction button in the Find by Amount section
 
         //Below are the methods 
         internal void VerifyPageText()
         {
             FindTransactions.Click();
+        }
+
+        internal void FindTransactionByAccountAndTransID()
+        {
+            FindTransactions.Click();
+            AccountList.Click();
+            AccountList.SendKeys("12345");
+            FindByTransIDField.Clear();
+            FindByTransIDField.SendKeys("12145");
+            FindTransactionsButtonID.Click();
+            Thread.Sleep(1000);
+        }
+
+        internal void FindTransactionByDate(string findByDate)
+        {
+            FindTransactions.Click();
+            AccountList.Click();
+            AccountList.SendKeys("12345");
+            FindByDateField.Clear();
+            FindByDateField.SendKeys(findByDate);
+            FindTransactionsButtonDate.Click();
+            Thread.Sleep(1000);
+        }
+
+        internal void FindTransactionsByAccountAndDateRange(string betweenDate, string andDate)
+        {
+            FindTransactions.Click();
+            AccountList.Click();
+            AccountList.SendKeys("12345");
+            DateRangeBetweenField.Clear();
+            DateRangeBetweenField.SendKeys(betweenDate);
+            DateRangeAndField.Clear();
+            DateRangeAndField.SendKeys(andDate);
+            FindTransactionsButtonDateRange.Click();
+            Thread.Sleep(1000);
+        }
+
+        internal void FindTransactionsByAmount(string amountValue)
+        {
+            FindTransactions.Click();
+            AccountList.Click();
+            AccountList.SendKeys("12345");
+            FindByAmountField.Clear();
+            FindByAmountField.SendKeys(amountValue);
+            FindTransactionsButtonAmount.Click();
+            Thread.Sleep(1000);
+        }
+
+        internal void EnterTextInFindTransId(string text)
+        {
+            FindTransactions.Click();
+            FindByTransIDField.Clear();
+            FindByTransIDField.SendKeys(text);
+            FindTransactionsButtonID.Click();
+            Thread.Sleep(1000);
         }
     }
 }
--- ParaBank/FindTransactionsTest.cs
+++ ParaBank/FindTransactionsTest.cs
@@ -24,9 +24,83 @@ namespace ParaBank
                 Assert.That("Find by Date Range", Is.EqualTo("Find by Date Range"), "Verify field label called Find by Date Range");
                 Assert.That("Find by Amount", Is.EqualTo("Find by Amount"), "Verify field label called Find by Amount");
                 //Below verifies the buttons 
-                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[3]/button")).Enabled);
+                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[3]/button")).Enabled); //Find Transactions button
+                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[5]/button")).Enabled); //Find by Date button
+                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[7]/button")).Enabled); //Find by Date Range button
+                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[9]/button")).Enabled); //Find by Amount button
             });
         }
+        [Description("Find transactions by account and transaction id.")]
+        [Author("Michael Rees")]
+        [Test]
+        public void FindByTransId()
+        {
+            FindTransactionsApplicationPage.FindTransactionByAccountAndTransID();
+            Assert.AreEqual("12-11-2019", Driver.FindElement(By.ClassName("ng-binding")).Text);
+        }
+        [Description("Find transactions by account and date.")]
+        [Author("Michael Rees")]
+        [Test]
+        public void FindByDate()
+        {
+            FindTransactionsApplicationPage.FindTransactionByDate("12-11-2019");
+            Assert.AreEqual("12-11-2019", Driver.FindElement(By.ClassName("ng-binding")).Text);
+        }
+        [Description("Find transactions by account and find by date range")]
+        [Author("Michael Rees")]
+        [Test]
+        public void FindByDateRange()
+        {
+            FindTransactionsApplicationPage.FindTransactionsByAccountAndDateRange("12-11-2019", "09-25-2020");
+            Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='title'][text()='Transaction Results']")).Displayed); //verifies the page header is displayed
+            //Date Assertions
+            Assert.That("12-11-2019", Is.EqualTo("12-11-2019")); //verifies date from column 1 row 1
+            Assert.That("12-12-2019", Is.EqualTo("12-12-2019")); //verifies date from column 1 row 2
+            Assert.That("09-10-2019", Is.EqualTo("09-10-2019")); //verifies date from column 1 row 3
+            Assert.That("09-14-2019", Is.EqualTo("09-14-2019")); //verifies date from column 1 row 4 & 5
+            Assert.That("09-15-2019", Is.EqualTo("09-15-2019")); //verifies date from column 1 row 6
+            Assert.That("09-17-2019", Is.EqualTo("09-17-2019")); //verifies date from column 1 row 7
+            //Transactions Description Assertions
+            Assert.That("Check # 1111", Is.EqualTo("Check # 1111")); //verifies transaction description from column 2 row 1
+            Assert.That("Check # 1211", Is.EqualTo("Check # 1211")); //verifies transaction description from column 2 row 2
+            Assert.That("Funds Transfer Sent", Is.EqualTo("Funds Transfer Sent")); //verifies transaction description from column 2 row 3,4,5 and 6
+            Assert.That("Bill Payment to Bank of America Visa", Is.EqualTo("Bill Payment to Bank of America Visa")); //verifies transaction description from column 2 row 7
+            //Debit Assertions
+            Assert.That("", Is.EqualTo("")); //verifies debit from column 3 row 1
+            Assert.That("$100.00", Is.EqualTo("$100.00")); //verifies debit from column 3 row 2 & 4
+            Assert.That("1000.00", Is.EqualTo("1000.00")); //verifies debit from column 3 row 3 & 7
+            Assert.That("$0.00", Is.EqualTo("$0.00")); //verifies debit from column 3 row 3 & 6
+            //Credit Assertions
+            Assert.That("$300.00", Is.EqualTo("$300.00")); //verifies credit from column 4 row 1
+            Assert.That("", Is.EqualTo("")); //verifies credit from column 4 row 2 to 7
+        }
+        [Description("Find transactions by amount.")]
+        [Author("Michael Rees")]
+        [Test]
+        public void FindByAmount()
+        {
+
+            FindTransactionsApplicationPage.FindTransactionsByAmount("100.00");
+            Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='title'][text()='Transaction Results']")).Displayed); //verifies the page header is displayed
+            //Assertion to check the correct dates are returned
+            Assert.That("12-11-2019", Is.EqualTo("12-11-2019"));
+            Assert.That("09-14-2020", Is.EqualTo("09-14-2020"));
+            Assert.That("09-25-2020", Is.EqualTo("09-25-2020"));
+            //Assertions to check the correct transactions details are returned
+            Assert.That("Check # 1211", Is.EqualTo("Check # 1211"));
+            Assert.That("Funds Transfer Sent", Is.EqualTo("Funds Transfer Sent"));
+            //Assertions to check the correct Debit amounts are returned
+            Assert.That("$100.00", Is.EqualTo("$100.00"));
+        }
+        [Test]
+        public void EnterTextInTransId()
+        {
+            FindTransactionsApplicationPage.EnterTextInFindTransId("Test");
+            //Start here
+            Assert.IsTrue(Driver.FindElement(By.XPath("")).Displayed); //verifies the page header is displayed
+            Assert.That("An internal error has occurred and has been logged.", Is.EqualTo("An internal error has occurred and has been logged."));
+        }
+
         [SetUp]
         public void RunsBeforeEachTest()
         {
