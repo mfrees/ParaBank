69a9f46579ce61bf9a70827fdbfc1914432155ec
Michael Rees <michaelrees@outlook.com>
2020-11-13 11:44:31 +0000

    Refactoring on FindtrasactionsTests

--- ParaBank/TestScenario's/FindTransactionsTest.cs
+++ ParaBank/TestScenario's/FindTransactionsTest.cs
@@ -16,18 +16,19 @@ namespace ParaBank
 
             Assert.Multiple(testDelegate: () =>
             {
-                Assert.AreEqual("ParaBank | Find Transactions", Driver.Title);
-                Assert.That("Find Transactions", Is.EqualTo("Find Transactions")); //Verifies the page title header text is displayed as expected
-                Assert.That("Select an account", Is.EqualTo("Select an account"), "Verify field label called Select an account");
-                Assert.That("Find by Transaction ID", Is.EqualTo("Find by Transaction ID"), "Verify field label called Find by Transaction ID");
-                Assert.That("Find by Date", Is.EqualTo("Find by Date"), "Verify field label called Find by Date");
-                Assert.That("Find by Date Range", Is.EqualTo("Find by Date Range"), "Verify field label called Find by Date Range");
-                Assert.That("Find by Amount", Is.EqualTo("Find by Amount"), "Verify field label called Find by Amount");
+                Assert.AreEqual("ParaBank | Find Transactions", Driver.Title); //This verifies the page title.
+                Assert.IsTrue(Driver.FindElement(By.CssSelector(".title")).Displayed);  //This verifies the page sub header.
+                //The following 5 lines verify the page labels text.
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='ng-pristine ng-valid ng-valid-required']//*[text()='Select an account']")).Displayed); 
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='ng-pristine ng-valid ng-valid-required']//*[text()='Find by Transaction ID']")).Displayed); 
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='ng-pristine ng-valid ng-valid-required']//*[text()='Find by Date']")).Displayed);
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='ng-pristine ng-valid ng-valid-required']//*[text()='Find by Date Range']")).Displayed);
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@class='ng-pristine ng-valid ng-valid-required']//*[text()='Find by Amount']")).Displayed);
                 //Below verifies the buttons 
-                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[3]/button")).Enabled); //Find Transactions button
-                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[5]/button")).Enabled); //Find by Date button
-                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[7]/button")).Enabled); //Find by Date Range button
-                Assert.IsTrue(Driver.FindElement(By.XPath("/html/body/div[1]/div[3]/div[2]/div/div/form/div[9]/button")).Enabled); //Find by Amount button
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@id='rightPanel']/div/div/form/div[3]/button")).Enabled); //Find Transactions button
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@id='rightPanel']/div/div/form/div[5]/button")).Enabled); //Find by Date button
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@id='rightPanel']/div/div/form/div[7]/button")).Enabled); //Find by Date Range button
+                Assert.IsTrue(Driver.FindElement(By.XPath("//*[@id='rightPanel']/div/div/form/div[9]/button")).Enabled); //Find by Amount button
             });
         }
         [Test]
